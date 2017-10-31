6aac5081a2d3257f5ae80cd8d4753937b39252da
rohithkrajan <rohithkrajan@gmail.com>
2017-10-31 11:08:25 +0530

    added comments PropertyRule

--- src/FinReconcile.Core/RuleEngine/Rules/PropertyRule.cs
+++ src/FinReconcile.Core/RuleEngine/Rules/PropertyRule.cs
@@ -6,8 +6,10 @@ using System.Web;
 
 namespace FinReconcile.Core.Engines.Rules
 {
+   // this is a simple rule which compares two property values of transaction 
     public class PropertyRule:IRule
     {
+         
         public string SourceProperty
         {
             get;
@@ -25,7 +27,8 @@ namespace FinReconcile.Core.Engines.Rules
             get;
             set;
         }
-
+        //these values are purposely kept as string to build a Rulebuilder UI
+        //We can also add an overload which will allow lambda expressions
         public PropertyRule(string SourceProperty, string Operator, string targetProperty)
         {
             this.SourceProperty = SourceProperty;
@@ -35,6 +38,8 @@ namespace FinReconcile.Core.Engines.Rules
 
         public Expression BuildExpression(ParameterExpression source,ParameterExpression target)
         {
+         //here it is quite trivail ato add support for other operator like Greaterthan,LessThan etc.
+         //currently only Equal is implemented as it was matching the document provided.
             return Expression.Equal(Expression.Property(source, this.SourceProperty),
                 Expression.Property(target, this.TargetProperty));
         }
